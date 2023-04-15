07957c9574076eea5bfb51c4f0bcadc7dc1b0e1d
Osama Alnuimi <osamaalnuimi@gmail.com>
2023-04-15 18:04:38 +0000

    Optimize Two sum solution

--- leetcode/easy/cSharp/TwoSum.cs
+++ leetcode/easy/cSharp/TwoSum.cs
@@ -16,4 +16,26 @@ public class Solution {
             }
             return new int[] { pointer, 0 };
         }
+
+          public int[] TwoSum_Optimization(int[] nums, int target)
+        {
+            int[] arr = new int[] { 3,3 };
+            int targetValue = 6;
+            // Create an empty dictionary
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+
+            // Iterate through the array and add each element to the dictionary
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int diff = targetValue -arr[i];
+                if (dict.ContainsKey(diff))
+                {
+                    return new int[] { dict[diff], i };
+
+                }
+                dict[arr[i]] = i;
+
+            }
+            return new int[] { 0, 0 };
+        }
 }
