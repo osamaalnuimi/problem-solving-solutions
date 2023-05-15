0e62788f3c8c7ed4475c1e7e7198e2981ebcf5c2
Osama Alnuimi <osamaalnuimi@gmail.com>
2023-05-16 00:46:40 +0300

    Create Index_Of_First_Occurence.cs
     Find the Index of the First Occurrence in a String

---
+++ leetcode/easy/cSharp/Index_Of_First_Occurence.cs
@@ -0,0 +1,8 @@
+public class Solution {
+    public int StrStr(string haystack, string needle) {
+       if (needle.Length == 0) {
+            return 0;
+        }
+        return haystack.IndexOf(needle);
+    }
+}
