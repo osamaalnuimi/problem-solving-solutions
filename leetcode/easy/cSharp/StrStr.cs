167e86fa989d0770d92faef27a69e118dd338f7d
Osama Alnuimi <osamaalnuimi@gmail.com>
2023-05-30 11:06:56 +0300

    Create StrStr.cs

---
+++ leetcode/easy/cSharp/StrStr.cs
@@ -0,0 +1,9 @@
+
+public class Solution {
+    public int StrStr(string haystack, string needle) {
+       if (needle.Length == 0) {
+            return 0;
+        }
+        return haystack.IndexOf(needle);
+    }
+}
