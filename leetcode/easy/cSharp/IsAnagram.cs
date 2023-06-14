d9277f87964a875659fb2802db3b1063088b0424
Osama Alnuimi <osamaalnuimi@gmail.com>
2023-06-14 21:57:39 +0300

    add another solution for IsAnagram problem

--- leetcode/easy/cSharp/IsAnagram.cs
+++ leetcode/easy/cSharp/IsAnagram.cs
@@ -5,4 +5,29 @@ public class Solution {
 
         return sLookUp.Count == tLookUp.Count && sLookUp.All(g => g.Count() == tLookUp[g.Key].Count());
     }
+    
+    public bool IsAnagram_Optimization(string s, string t)
+    {
+        if (s == t) return true;
+        if (t.Length != s.Length) return false;
+
+        Dictionary<char,int> sDic = new Dictionary<char,int>();
+        Dictionary<char,int> tDic = new Dictionary<char,int>();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            sDic[s[i]] = 1 + (sDic.ContainsKey(s[i]) ? sDic[s[i]]: 0 );
+            tDic[t[i]] = 1 + (tDic.ContainsKey(t[i]) ? tDic[t[i]]: 0 );
+        }
+        foreach (char item in sDic.Keys)
+        {
+
+            if (!tDic.ContainsKey(item) ||  sDic[item] != tDic[item])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    
 }
